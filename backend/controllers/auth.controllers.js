98d285c2d7b73e5046b96e6725cd16afe2275749
priyanshkhas <priyanshkhasdev@gmail.com>
2025-10-07 00:30:57 +0530

    Auth Controller Edited

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -28,7 +28,7 @@ export const signUp = async (req, res) => {
        res.cookie("token",token  ,{
         httpOnly: true,
         maxAge: 7*24*60*60*1000, // 7 days
-        samesize: "None",
+        sameSite: "Strict",
         secure: false, // set to true in production
        }) 
 
@@ -65,7 +65,7 @@ export const login = async (req, res) => {
        res.cookie("token",token  ,{
         httpOnly: true,
         maxAge: 7*24*60*60*1000, // 7 days
-        samesize: "None",
+        sameSite: "Strict",
         secure: false, // set to true in production
        }) 
 
