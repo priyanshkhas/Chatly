8baa99886211ae8ac6ca1ce6e5240b1a4d9461e2
priyanshkhas <priyanshkhasdev@gmail.com>
2025-11-13 23:29:03 +0530

    All pages Setup Properly

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import SideBar from '../components/SideBar';
-import MessageArea from '../components/MessageArea';
-import { useSelector } from 'react-redux';
-import getMessages from '../customHooks/getMessages';
+import SideBar from '../components/SideBar'
+import MessageArea from '../components/MessageArea'
+import { useSelector } from 'react-redux'
+import getMessage from '../customHooks/getMessages'
+
 
 function Home() {
-  let {selectedUser} = useSelector(state => state.user)
-  getMessages()
+  let {selectedUser}=useSelector(state=>state.user)
+ getMessage()
   return (
-    <div className='w-full h-[100vh] flex overflow-hidden'>
-      <SideBar/>
-      <MessageArea/>
-        
+    <div className='w-full h-[100vh] flex  '>
+     <SideBar/>
+     <MessageArea/>
     </div>
-  );
+  )
 }
 
-export default  Home
+export default Home
  
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,100 +1,62 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { serverUrl } from '../main';
-import {useDispatch, useSelector} from 'react-redux'
-import { setUserData } from '../redux/userSlice';
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { serverUrl } from '../main'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUserData } from '../redux/userSlice'
+
 function SignUp() {
-  const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [err, setErr] = useState(false);
-  const dispatch = useDispatch()
-  const handleSignUp = async (e) => {
-    e.preventDefault();
-    setLoading(true)
-    try {
-      const result = await axios.post(`${serverUrl}/api/auth/signup`, {
-        userName,
-        email,
-        password,
-      }, {
-        withCredentials: true,
-      });
-      dispatch(setUserData(result.data))
-      console.log("Signup success:", result.data); //remove
-      navigate("/profile");
-      setEmail("")
-      setPassword("")
-      setLoading(false)
-      setErr("")
-    } catch (error) {
-      console.error("Signup error:", error.response?.data || error.message);
-      setLoading(false)
-      setErr(error?.response?.data?.message)
+    let navigate=useNavigate()
+    let [show,setShow]=useState(false)
+let [userName,setUserName]=useState("")
+let [email,setEmail]=useState("")
+let [password,setPassword]=useState("")
+let [loading,setLoading]=useState(false)
+let [err,setErr]=useState("")
+let dispatch=useDispatch()
+
+    const handleSignUp=async (e)=>{
+        e.preventDefault()
+        setLoading(true)
+        try {
+            let result =await axios.post(`${serverUrl}/api/auth/signup`,{
+userName,email,password
+            },{withCredentials:true})
+           dispatch(setUserData(result.data))
+           navigate("/profile")
+            setEmail("")
+            setPassword("")
+            setLoading(false)
+            setErr("")
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+            setErr(error?.response?.data?.message)
+        }
     }
-  };
 
   return (
     <div className='w-full h-[100vh] bg-slate-200 flex items-center justify-center'>
-      <div className='w-full max-w-[500px] h-[600px] bg-white rounded-lg shadow-gray-400 shadow-lg flex flex-col gap-[30px]'>
+     <div className='w-full max-w-[500px] h-[600px] bg-white rounded-lg shadow-gray-400 shadow-lg flex flex-col gap-[30px]'>
         <div className='w-full h-[200px] bg-[#20c7ff] rounded-b-[30%] shadow-gray-400 shadow-lg flex items-center justify-center'>
-          <h1 className='text-gray-600 font-bold text-[30px]'>
-            Welcome to <span className='text-white'>chatly</span>
-          </h1>
+           <h1 className='text-gray-600 font-bold text-[30px]'>welcome to <span  className='text-white'>chatly</span></h1>
         </div>
+        <form className='w-full flex flex-col gap-[20px] items-center' onSubmit={handleSignUp}>
 
-        <form className="w-full flex flex-col gap-[20px] items-center" onSubmit={handleSignUp}>
-          <input
-            type="text"
-            placeholder="username"
-            className="w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-white rounded-lg shadow-lg text-gray-700 text-[19px]"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-
-          <input
-            type="email"
-            placeholder="email"
-            className="w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-white rounded-lg shadow-lg text-gray-700 text-[19px]"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <div className="w-[90%] h-[50px] border-2 border-[#20c7ff] overflow-hidden rounded-lg shadow-lg relative">
-            <input
-              type={show ? "text" : "password"}
-              placeholder="password"
-              className="w-full h-full outline-none px-[20px] py-[10px] bg-white text-gray-700 text-[19px]"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <span
-              className="absolute top-[12px] right-[20px] text-[20px] text-[#20c7ff] font-semibold cursor-pointer"
-              onClick={() => setShow(prev => !prev)}
-            >
-              {show ? "hide" : "show"}
-            </span>
-          </div>
-          {err && <p className='text-red-500'>{"*"+err}</p>}
-          <button
-            className="px-[20px] py-[10px] bg-[#20c7ff] rounded-2xl shadow-gray-400 shadow-lg text-[20px] w-[200px] mt-[20px] font-semibold hover:shadow-inner"
-            disabled={loading}
-          >
-            {loading?"Loading...":"Sign Up"}
-          </button>
-
-          <p className="cursor-pointer" onClick={() => navigate("/login")}>
-            Already Have An Account?{' '}
-            <span className="text-[#20c7ff] font-bold">Login</span>
-          </p>
-        </form>
-      </div>
+        <input type="text" placeholder='username' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]' onChange={(e)=>setUserName(e.target.value)} value={userName}/>
+        <input type="email" placeholder='email' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]'  onChange={(e)=>setEmail(e.target.value)} value={email}/>
+        <div className='w-[90%] h-[50px] border-2 border-[#20c7ff] overflow-hidden rounded-lg shadow-gray-200 shadow-lg relative'>
+        <input type={`${show?"text":"password"}`} placeholder='password' className='w-full h-full outline-none  px-[20px] py-[10px] bg-[white]  text-gray-700 text-[19px]'  onChange={(e)=>setPassword(e.target.value)} value={password}/>
+        <span className='absolute top-[10px] right-[20px] text-[19px] text-[#20c7ff] font-semibold cursor-pointer' onClick={()=>setShow(prev=>!prev)}>{`${show?"hidden":"show"}`}</span>
+        </div>
+        {err && <p className='text-red-500'>{"*" + err}</p>}
+        <button className='px-[20px] py-[10px] bg-[#20c7ff] rounded-2xl shadow-gray-400 shadow-lg text-[20px] w-[200px] mt-[20px] font-semibold hover:shadow-inner' disabled={loading}>{loading?"Loading...":"Sign Up"}</button>
+        <p className='cursor-pointer' onClick={()=>navigate("/login")}>Already Have An Account ? <span className='text-[#20c7ff] text-[bold]' >Login</span></p>
+     </form>
+     </div>
+     
     </div>
-  );
+  )
 }
 
-export default SignUp;
+export default SignUp
