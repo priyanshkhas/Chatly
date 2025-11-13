4752633183ad3cb17265e8da3e25f1f53466f775
priyanshkhas <priyanshkhasdev@gmail.com>
2025-11-13 23:33:13 +0530

    Edited

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import './index.css';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-
-export const serverUrl = "http://localhost:8000"; 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
-import { store } from './redux/store.js';
+import { store } from './redux/store.js'
 
+export const serverUrl="http://localhost:8000"
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter> 
-  </React.StrictMode> 
-);
-                                    
+<BrowserRouter>
+<Provider store={store}>
+    <App />
+</Provider>
+</BrowserRouter>
+ 
+)                                
