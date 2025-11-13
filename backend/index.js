c77cc5b9a00f903dcd9140d61a445f829c98ab51
priyanshkhas <148960031+priyanshkhas@users.noreply.github.com>
2025-11-14 00:01:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatly-qd70.onrender.com",
     credentials:true
 }))
 app.use(express.json())
